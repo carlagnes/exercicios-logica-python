70589bd6400c17ea0c1540405595c9c87c3acc15
Carla Agnes <carlaagnes28@gmail.com>
2025-07-25 23:14:53 -0300

    Add exercicio verifica maior

---
+++ estruturas-de-selecao/07-num_maior.py
@@ -0,0 +1,11 @@
+# Escreva um algoritmo que receba dois valores inteiros distintos e indique qual deles é o maior.
+
+print("\n===== VERIFICA MAIOR NÚMERO =====")
+num1 = int(input("\nDigite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1>num2:
+  print(f"\nO número {num1} é maior que {num2}.")
+else:
+  print(f"\nO número {num2} é maior que {num1}.")
+
